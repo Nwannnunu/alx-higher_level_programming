cc72a7d31214a5baa260c0fcc7403906ac7d67e5
Nwannnunu <ezinnevirtous@gmail.com>
2022-07-04 13:29:57 +0000

    print maximum number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,7 +6,7 @@ def max_integer(my_list=[]):
     if count == 0:
         return None
     biggest = my_list[0]
-    for i in range(0,len(my_list)):
+    for i in range(0, len(my_list)):
         if my_list[i] > biggest:
             biggest = my_list[i]
     return biggest
