af1672d1a8cc7db03558c46168bd69e4565ceb36
Nwannnunu <ezinnevirtous@gmail.com>
2022-07-04 12:17:12 +0000

    print maximum number

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,6 +7,6 @@ def max_integer(my_list=[]):
         return None
     v_max = my_list[0]
     for i in my_list:
-        if i > v_max:
+        if i >= v_max:
             v_max = i
         return v_max
