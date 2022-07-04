ac0b682114f2e4937670a4429eb3d5e50bdfe624
Nwannnunu <ezinnevirtous@gmail.com>
2022-07-04 09:09:47 +0000

    print maximum value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -5,8 +5,8 @@ def max_integer(my_list=[]):
     count = len(my_list)
     if count == 0:
         return None
-    mx_value = my_list[0]
+    v_max = my_list[0]
     for i in my_list:
-        if i > mx_value:
-            mx_value = i
-        return mx_value
+        if i > v_max:
+            v_max = i
+        return v_max
