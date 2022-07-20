86cb197023f4975c29e9c08ea5ce70fd9c5d87de
Nwannnunu <ezinnevirtous@gmail.com>
2022-07-20 00:00:40 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""private instance attribute"""
+
+
+class square(object):
+    """class Variable size"""
+    def __init__(self, size):
+        """initialize size"""
+        self.__size = size
