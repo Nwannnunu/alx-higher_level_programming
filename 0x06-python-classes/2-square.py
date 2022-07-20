9f21798bdd6ac98d04bbca4620c52edfab515f41
Nwannnunu <ezinnevirtous@gmail.com>
2022-07-20 00:17:04 +0000

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square(object):
     def __init__(self, size=0):
         """initialize size"""
         if type(size) != int:
-            raise TypeError("size must be aninteger")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
